35deca07803b36ce38a01032358248f12d0a2b1b
Alex <jimenezs.alex@gmail.com>
2021-12-09 12:51:21 +0100

    ft_get_map

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: ajimenez <ajimenez@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/12/04 15:26:16 by ajimenez          #+#    #+#              #
-#    Updated: 2021/12/09 10:18:09 by ajimenez         ###   ########.fr        #
+#    Updated: 2021/12/09 12:49:34 by ajimenez         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,11 +22,13 @@ LIBMLX	= ./includes/mlx/libmlx.a
 LIBP	= so_long.a 
 
 NAME 	= so_long
-SRCS	= srcs/main.c srcs/key_parser.c srcs/map_checker.c 
+
+MAP 	= srcs/map/get_map.c
+SRCS	= srcs/main.c srcs/key_parser.c $(MAP) 
 
 OBJS	= $(SRCS:.c=.o)
 COMP	= $(CC) $(CFLAGS) $(SRCS) $(LIBFT) $(LIBMLX) $(MLXFLGS) -o $(NAME)
-SANCOMP	= $(CC) $(CFLAGS) $(SAN) $(LIBFT) $(LIBP) $(SRCS) -g3 -o $(NAME)
+SANCOMP	= $(CC) $(CFLAGS) $(SAN) $(LIBFT) $(LIBP) $(SRCS) $(LIBMLX) $(MLXFLGS) -g3 -o $(NAME)
 
 all: 		$(NAME)
 
@@ -44,8 +46,8 @@ fsanitize:			$(OBJS)
 					@echo "\n\033[33mMaking libft! ░░░░░░ /(ಠ_ಠ)\ ░░░░░\033[39m\n"
 					@make -sC ./includes/libft
 					@make -C ./includes/mlx
-					@cp /includes/mlx/libmlx.a ./
-					@cp /includes/libft/libft.a ./
+					@cp ./includes/mlx/libmlx.a ./
+					@cp ./includes/libft/libft.a ./
 					$(LIB) $(LIBP) $(OBJS)
 					$(SANCOMP)
 					@echo "\n\033[1;32mEverything done! ░░░░░░ ＼(>o<)ノ ░░░░░\033[39m\n"
--- srcs/map/get_map.c
+++ srcs/map/get_map.c
@@ -1,28 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   map_checker.c                                      :+:      :+:    :+:   */
+/*   get_map.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 17:29:11 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/12/09 12:14:45 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/12/09 12:50:09 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/so_long.h"
+#include "../../includes/so_long.h"
 
-//static void ft_strcpyalloc(char *dst, const char *src)
-//{
-//	dst = ft_calloc(ft_strlen(src), sizeof(char));
-//	ft_strcpy(dst, src);
-//}
-
-char	**check_map(char **av)
+static size_t ft_lines_of_map(char **av)
 {
 	int		fd;
 	char	*line;
-	char	**map;
 	size_t	i;
 
 	i = 0;
@@ -35,27 +28,27 @@ char	**check_map(char **av)
 		i++;
 	}
 	close(fd);
-	map = ft_calloc(i + 1, sizeof(char *));
+	return (i + 1);
+}
+
+char	**check_map(char **av)
+{
+	int		fd;
+	char	*line;
+	char	**map;
+	size_t	i;
+
 	i = 0;
+	map = ft_calloc(ft_lines_of_map(av), sizeof(char *));
 	fd = open(av[1], O_RDONLY);
 	line = get_next_line(fd);
-//	int j;
-//	j = 0;
 	while (line)
 	{
-		//j = 0;
 		map[i] = ft_calloc(ft_strlen(line) + 1, sizeof(char));
-	//	while (line[j])
-	//	{
-	//		map[i][j] = line[j];
-	//		j++;
-	//	}
-	//		map[i][j] = '\0';
 		ft_strcpy(map[i], line);
-		printf("%zu-- %zu %s\n", i, ft_strlen(line), map[i]);
 		free(line);
 		line = get_next_line(fd);
 		i++;
 	}
-	return (0);
+	return (map);
 }
